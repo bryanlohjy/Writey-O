5cdc5825788f32ec5eb33a602c43c15957e0c2b7
bryanlohjy <bryanlohjy@hotmail.com>
2016-10-06 15:32:48 +1300

    started passing props to subcomponents

--- components/write.jsx
+++ components/write.jsx
@@ -26,69 +26,69 @@
 // });
 
 // list of prompts
-var prompts = [
-  {prompt: "write the letter yay"},
-  {prompt: "write the cat yay"},
-  {prompt: "segsg"},
-  {prompt: "wty"}
-];
+// var prompts = [
+//   {prompt: "write the letter yay"},
+//   {prompt: "write the cat yay"},
+//   {prompt: "segsg"},
+//   {prompt: "wty"}
+// ];
 
 
 
 // WRITEYO --------------------------------------------------------------
 // Countdown timer
-var Timer = React.createClass({
-  getInitialState: function() {
-    return {
-    	promptNo: 1,
-    	currentPrompt: prompts[0].prompt,
-    	timeRemaining: 15
-    };
-  },
-  tick: function() {
-  	// countdown
-    this.setState({
-    	timeRemaining: this.state.timeRemaining - 1
-    });
-    // if out of time 
-  	if (this.state.timeRemaining == 0){
-  		  this.resetCount();
-        this.advancePromptNo();
-  	}
-  },
-  resetCount: function() {
-    this.setState({
-      timeRemaining: 15
-    });
-  },
-  advancePromptNo: function() {
-    this.setState({
-      promptNo: this.state.promptNo + 1
-    });
-    this.advancePrompt();
-  },
-  advancePrompt: function(){
-    this.setState({
-      currentPrompt: prompts[this.state.promptNo].prompt
-    });
-  },
-  componentDidMount: function() {
-  	// countdown every second
-    this.interval = setInterval(this.tick, 1000);
-  },
-  componentWillUnmount: function() {
-    clearInterval(this.interval);
-  },
-  render: function() {
-    return (
-    <div>
-      <div>Time Remaining: {this.state.timeRemaining}</div>
-      <div>Prompt number: {this.state.promptNo}</div>
-      <div>Prompt: {this.state.currentPrompt}</div>
-    </div>
-    );
-  }
-});
+// var Timer = React.createClass({
+//   getInitialState: function() {
+//     return {
+//     	promptNo: 1,
+//     	currentPrompt: prompts[0].prompt,
+//     	timeRemaining: 15
+//     };
+//   },
+//   tick: function() {
+//   	// countdown
+//     this.setState({
+//     	timeRemaining: this.state.timeRemaining - 1
+//     });
+//     // if out of time 
+//   	if (this.state.timeRemaining == 0){
+//   		  this.resetCount();
+//         this.advancePromptNo();
+//   	}
+//   },
+//   resetCount: function() {
+//     this.setState({
+//       timeRemaining: 15
+//     });
+//   },
+//   advancePromptNo: function() {
+//     this.setState({
+//       promptNo: this.state.promptNo + 1
+//     });
+//     this.advancePrompt();
+//   },
+//   advancePrompt: function(){
+//     this.setState({
+//       currentPrompt: prompts[this.state.promptNo].prompt
+//     });
+//   },
+//   componentDidMount: function() {
+//   	// countdown every second
+//     this.interval = setInterval(this.tick, 1000);
+//   },
+//   componentWillUnmount: function() {
+//     clearInterval(this.interval);
+//   },
+//   render: function() {
+//     return (
+//     <div>
+//       <div>Time Remaining: {this.state.timeRemaining}</div>
+//       <div>Prompt number: {this.state.promptNo}</div>
+//       <div>Prompt: {this.state.currentPrompt}</div>
+//     </div>
+//     );
+//   }
+// });
 
 
 
@@ -105,29 +105,97 @@ var Timer = React.createClass({
 // INPUT ---------------------------------------------------------------
 // Inputs
 
-// Form
-var Form = React.createClass({
-	render: function() {
-		return (
-				<textarea></textarea>
-		)
-	}
+
+
+// Full page -------------------------------------------------------------
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      promptNo: 1,
+      timeRemaining: 15
+    }
+  },
+  tick: function() {
+    // countdown
+    this.setState({
+      timeRemaining: this.state.timeRemaining - 1
+    });
+    // if out of time 
+    if (this.state.timeRemaining == 0){
+        this.resetCount();
+        this.advancePromptNo();
+    }
+  },
+  resetCount: function() {
+    this.setState({
+      timeRemaining: 15
+    });
+  },
+  advancePromptNo: function() {
+    this.setState({
+      promptNo: this.state.promptNo + 1
+    });
+  },
+  componentDidMount: function() {
+    // countdown every second
+    this.interval = setInterval(this.tick, 1000);
+
+    // applying styles
+    fullHeight('writeyO');
+    fullHeight('writeInput');
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+  render: function() {
+    return (
+        <div>
+          <div className= "six columns" id="writeyO">
+            <Timer timeRemaining={this.state.timeRemaining}/>
+            <Prompt promptNo = {this.state.promptNo}/>
+          </div>
+
+          <div className= "six columns" id="writeInput">
+            <Writing />
+          </div>
+        </div>
+    )
+  }
 });
 
-//  Compiling Left side of App (OUTPUT) ------------------------------------
-var AppLeft = React.createClass({
-	componentDidMount: function() {
-		fullHeight('writeyO');
-	},
-	render: function() {
-		return (
-			<div className= "six columns" id="writeyO">
-				<Timer />
-			</div>
 
-		)
-	}
+var Timer = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <div>Time Remaining: {this.props.timeRemaining}</div>
+      </div>
+    )
+  }
 });
+
+
+var Prompt = React.createClass({
+  getInitialState: function(){
+    return {
+      prompts : [
+        "write the letter a",
+        "write the letter b",
+        "write the letter c",
+        "write the letter d"
+      ]
+    }
+  },
+  render: function(){
+    return (
+      <div>
+        <div>{this.props.promptNo}</div>
+        <div>{this.state.prompts[this.props.promptNo-1]}</div>
+      </div>
+    )
+  }
+});
+
 
 // Adapted from React documentation example
 var Entry = React.createClass({
@@ -166,52 +234,4 @@ var Writing = React.createClass({
   }
 });
 
-// Compiling Right side of App (INPUT) ------------------------------------
-var AppRight = React.createClass({
-  componentDidMount: function() {
-    fullHeight('writeInput');
-  },
-  render: function() {
-    return (
-      <div className= "six columns" id="writeInput">
-        <Writing />
-      </div>
-    )
-  }
-});
-
-// Full page -------------------------------------------------------------
-var App = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <AppLeft />
-        <AppRight />
-      </div>
-    )
-  }
-
-});
-
-
-
-
-ReactDOM.render(<App />,
-  document.getElementById('body'));
-
-//   render() {
-//     return (
-//       <div>
-//         <SiblingA
-//           myProp={this.state.propA}
-//           myFunc={this.siblingAFunc.bind(this)}
-//           />
-//         <SiblingB
-//           myProp={this.state.propB}
-//           myFunc={this.siblingBFunc.bind(this)}
-//           />
-//       </div>
-//     );
-//   }
-//   // Define 'siblingAFunc' and 'siblingBFunc' here 
-// }
+ReactDOM.render(<App />, document.getElementById('body'));
